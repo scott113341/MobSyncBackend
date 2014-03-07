fca8da903591dc9f03884767f621e0aa0747eefe
Scott Hardy <scott.the.hardy@gmail.com>
2014-03-06 21:16:00 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobSyncBackend::Application.config.secret_key_base = 'b4ac19bc201da458c6793fd3a537b4901a32c13415223434ec68816260e0aa7d7b39fcb5c37406a7e8b0fcc84acaf7cdcb13157a949afeb94f0cba4d8e0b1588'
