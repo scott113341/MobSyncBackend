261c81da4c94378b00541f34177ca0ad4497c64e
Scott Hardy <scott.the.hardy@gmail.com>
2014-03-09 00:28:39 -0800

    working on handler

--- app/models/mob.rb
+++ app/models/mob.rb
@@ -1,5 +1,16 @@
+require 'apns'
+
 class Mob < ActiveRecord::Base
+  before_create :send_push
+
   def users
     User.find(self.user_ids.split(','))
+  end
+
+  private
+
+  def send_push
+    puts 'meowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+    puts self.attributes.to_s
   end
 end
